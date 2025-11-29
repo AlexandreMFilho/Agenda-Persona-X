134d02a2d0688bd42a746aa00dd8e99ebe96408f
alexmaimartfilho <alexmaimartfilho@gmail.com>
2025-11-29 01:14:10 -0300

    Agenda ta ganhando forma. APRENDI ALGO NOVOgit add .! Children em componentes proprios

--- AgendaPersona/src/components/Agenda.jsx
+++ AgendaPersona/src/components/Agenda.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { useGame } from '../context/GameContext';
 
 import { Card } from 'primereact/card';
+import Divisoria from './Divisoria';
 
 
 const Agenda = ({ }) => {
@@ -9,25 +10,39 @@ const Agenda = ({ }) => {
 
     return (
         <>
-            {/* <h2>Agenda</h2> */}
             <div
-                style={{display: 'flex', flexDirection: 'column', flexWrap: 'wrap', height: '95%',
-                    border: '1px solid #000000ff', padding: '10px', borderRadius: '10px'
-                }}
-                className='fundo'
-                >
-                    <div
-                        style={{border: '1px solid #000000ff', height: '50%', width: '100%' }}   
-                        >
-                        1
-                    </div>
-                    <div
-                        style={{border: '1px solid #000000ff', height: '50%', width: '100%'}}
-                        >
-                        2
-                    </div>
+                className='fundoAgenda'
+            >
+                <Divisoria altura={6}>
+                    <div className='timeAgenda'></div>
+                    <div className='diasAgenda'>Mon</div>
+                    <div className='diasAgenda'>Tue</div>
+                    <div className='diasAgenda'>Wed</div>
+                    <div className='diasAgenda'>Thu</div>
+                    <div className='diasAgenda'>Fri</div>
+                    <div className='diasAgenda'>Sat</div>
+                </Divisoria>
+                <Divisoria altura={47} >
+                    <div className='timeAgenda'>Daytime</div>
+                    <div className='colunaAgenda'>1</div>
+                    <div className='colunaAgenda'>2</div>
+                    <div className='colunaAgenda'>3</div>
+                    <div className='colunaAgenda'>4</div>
+                    <div className='colunaAgenda'>5</div>
+                    <div className='colunaAgenda'>6</div>
+                </Divisoria>
+                <Divisoria altura={47} >
+                    <div className='timeAgenda'>Evening</div>
+                    <div className='colunaAgenda'>1</div>
+                    <div className='colunaAgenda'>2</div>
+                    <div className='colunaAgenda'>3</div>
+                    <div className='colunaAgenda'>4</div>
+                    <div className='colunaAgenda'>5</div>
+                    <div className='colunaAgenda'>6</div>
+                </Divisoria>
+
             </div>
-            
+
         </>
     );
 };
--- AgendaPersona/views/home.jsx
+++ AgendaPersona/views/home.jsx
@@ -2,29 +2,29 @@ import React, { useState, useEffect } from 'react';
 import { useGame } from '../src/context/GameContext';
 import Agenda from '../src/components/Agenda';
 
+import { Dropdown } from 'primereact/dropdown';
+        
 const Home = () => {
   const { game, chooseGame } = useGame();
   useEffect(() => {
-    document.documentElement.className = `${game} ${theme}`;
-
+    document.documentElement.className = `${game} `;
   }, [game]);
-
+  const jogos = [ 'p3', 'p4', 'p5', 'p6' ];
+  
 
   return (
     <>
-      <div style={{ width: '100vh', height: '100vh', border: '1px solid #ccc', padding: '20px' }}
+      <div style={{ width: '100vh', height: '100vh', padding: '20px' }}
         // className={`${game}`}
       >
 
         {/* <Cabecalho /> */}
         {/* <h1>Welcome to Agenda {game}</h1> */}
         <div className={`${game}`}>
-          <input
-            type="text"
-            value={game}
-            onChange={(e) => chooseGame(e.target.value)}
-          />
-
+          
+          <Dropdown value={game} onChange={(e) =>chooseGame(e.target.value)} options={jogos} optionLabel="code" 
+            placeholder="Selecione um jogo" className="w-full md:w-14rem" checkmark={true}  highlightOnSelect={false} />
+         
         </div>
         <Agenda />
         {/* <Rodape /> */}
